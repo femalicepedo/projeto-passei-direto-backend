f6c4e66665ad153f3afc8de1b0aacf45e557d418
femalicepedo <femalicepedo@gmail.com>
2019-09-09 23:30:52 -0300

    First Commit

---
+++ scripts/create-database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE projetopasseidireto;
+
+USE projetopasseidireto;
+
+-- Rodar
+-- * npm sequelize db:migrate
+-- ou
+-- * yarn sequelize db:migrate
+-- para criação de todas as tabelas e associações
+--
